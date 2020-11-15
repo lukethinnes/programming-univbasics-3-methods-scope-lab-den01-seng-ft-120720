aa5f20117ce56b07beef7140d3c6911fff04048e
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-15 19:39:27 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
 def mario
   phrase = "It's-a me, Mario!"
-
   puts phrase
 end
-$status = 'Thank you Mario! But Out Princess Is In Another Castle!'
+
 
 def toadstool
+  status = 'Thank you Mario! But Out Princess Is In Another Castle!'
   puts status
 end
